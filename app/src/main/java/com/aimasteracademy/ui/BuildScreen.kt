db1b854d04d7b29095a579cf8035849433036923
masonrypros89-wq <masonrypros89@gmail.com>
2026-07-02 19:01:43 -0500

    Add BuildScreen composable for build mode

---
+++ app/src/main/java/com/aimasteracademy/ui/BuildScreen.kt
@@ -0,0 +1,10 @@
+package com.aimasteracademy.ui
+
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun BuildScreen(navController: NavController) {
+    Text("Build Mode - Coming Next Phase")
+}
