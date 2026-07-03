12cfd46c20a8226d985bf50a34619316d038bae7
masonrypros89-wq <masonrypros89@gmail.com>
2026-07-02 19:00:11 -0500

    Add LearnScreen composable for learning mode

---
+++ app/src/main/java/com/aimasteracademy/ui/LearnScreen.kt
@@ -0,0 +1,10 @@
+package com.aimasteracademy.ui
+
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun LearnScreen(navController: NavController) {
+    Text("Learn Mode - Coming Next Phase")
+}
